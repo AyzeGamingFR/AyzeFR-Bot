c7b0ff583496ec4adfec18e24675b1cfb22c573f
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2020-07-02 17:07:51 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,18 @@
+//dependances
+
+var Discord = require("discord.js");
+var fs = require("fs");
+
+
+//variables de base
+
+var bot = new Discord.Client();
+
+
+//bot.on('ready', ())
+
+bot.on('ready', () => {
+	
+	
+	
+});
