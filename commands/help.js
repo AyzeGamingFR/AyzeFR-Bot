0b9948268490e63d5c6ed67a076b064fe15c7443
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2020-08-02 22:27:12 +0200

    Updated help.js

--- commands/help.js
+++ commands/help.js
@@ -9,8 +9,13 @@ var helpMessageEmbed = new Discord.RichEmbed()
   helpMessageEmbed.setColor("#")
   helpMessageEmbed.setTitle("**Help Page :**")
   helpMessageEmbed.addField("")
-
+  
 var helpPmMessageEmbed = new Discord.RichEmbed()
   helpPmMessageEmbed.setColor("#")
   helpPmMessageEmbed.setTitle("")
   helpPmMessageEmbed.addField("")
+  
+var staffMembersListMessageEmbed = new discord.RichEmbed()
+  staffMembersListMessageEmbed.setColor("#")
+  staffMembersListMessageEmbed.setTitle("")
+  staffMembersListMessageEmbed.addField()
