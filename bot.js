f94d4035106e3e544a99beaed292e6f6d773c135
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2020-08-02 02:05:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,30 @@
-//dependances
+// dependancies
 
 var Discord = require("discord.js");
 var fs = require("fs");
 
 
-//variables de base
+// default functions
 
 var bot = new Discord.Client();
 
 
-//bot.on('ready', ())
+// bot.on('ready', ())
 
 bot.on('ready', () => {
 	
+	console.log("");
 	
+	var botReadEmbed = new Discord.RichEmbed()
+		botReadEmbed.setColor("#");
+		botReadEmbed.setAuthor({"name": "",
+"id": "533016846031650826"});
+		botReadEmbed.setTitle("");
+		botReadEmbed.addField("")
+	
+	msg.channel({"name": "📰ayzefrbot-logs",
+"id": ""}).send(botReadEmbed)
 	
 });
+
+bot.login(botToken);
