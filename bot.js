c6195f08ec52e697329bc45f409a5490976a458c
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2020-08-02 02:18:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,21 @@
 var Discord = require("discord.js");
 var fs = require("fs");
 
+var http = require("http");
+var url = require("url");
+
 
 // default functions
 
 var bot = new Discord.Client();
+
+var botStartedYear = Date.getYear();
+var botStartedMonth = Date.getMonth();
+var botStartedDay = Date.getDay();
+var botStartedHour = Date.getHours();
+var botStartedMinute = Date.getMinutes();
+var botStartedSecond = Date.getSecond();
+var botStartedMillisecond = Date.getMillisecond();
 
 
 // bot.on('ready', ())
@@ -16,14 +27,29 @@ bot.on('ready', () => {
 	console.log("");
 	
 	var botReadEmbed = new Discord.RichEmbed()
-		botReadEmbed.setColor("#");
+		botReadEmbed.setColor("#")
 		botReadEmbed.setAuthor({"name": "",
-"id": "533016846031650826"});
-		botReadEmbed.setTitle("");
-		botReadEmbed.addField("")
+"id": "533016846031650826"})
+		botReadEmbed.setTitle("**I am ready to work !**")
+		botReadEmbed.addField(`I was started the ${botStartedDay}:${botStartedMonth}:${botStartedYear} at ${botStartedHour}:${botStartedMinute}:${botStartedSecond}:${botStartedMillisecond} !`)
 	
 	msg.channel({"name": "📰ayzefrbot-logs",
 "id": ""}).send(botReadEmbed)
+	
+});
+
+bot.on("message", msg => {
+	
+	if (msg.content === botPrefix +"help") {
+		
+		var helpMessageEmbed = new Discord.RichEmbed()
+			helpMessageEmbed.setColor("#")
+			helpMessageEmbed.setAuthor({"name": "AyzeFR",
+"id": "533016846031650826"})
+			helpMessageEmbed.setTitle("**Help Page :**")
+			helpMessageEmbed.addField("")
+		
+	}
 	
 });
 
